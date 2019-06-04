c73786eb54f029cc8387ae2013190c9eb648e8b5
Timoté Dallaporta <Timot Dallaporta@Timote-Dallaporta>
2019-06-04 18:55:22 +0200

    modif main
    instanciate model

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -23,12 +23,6 @@ public abstract class Main {
      *            the arguments
      */
     public static void main(final String[] args) {
-        final Model model = new Model();
-        final View view = new View(model);
-        final Controller controller = new Controller(view, model);
-        view.setController(controller);
-
-        controller.control();
-        controller.orderPerform(ControllerOrder.English);
+    	Model model = new Model();
     }
 }
